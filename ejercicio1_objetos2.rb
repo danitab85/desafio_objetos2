ebfea25a5580c5c480a5f2697ec0618717d2278b
danitab85 <danielaballesteros85@gmail.com>
2019-11-02 17:57:57 -0300

    Corrección ejercicio 1)

--- ejercicio1_objetos2.rb
+++ ejercicio1_objetos2.rb
@@ -1,14 +1,15 @@
 class Anything
-  attr_accessor :a
-  def initialize
-    @a=5
-  end
-
-  def foo
-    a = 5
-  end
- 
-  def bar
-    @a += 1
-  end
+ def foo
+   @a = 5
+ end
+ def bar
+   @a += 1
+ end
+ def a
+ puts @a
+ end
 end
+any = Anything.new
+any.foo
+any.bar
+any.a
